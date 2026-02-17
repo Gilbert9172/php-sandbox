d70f132b5b00d7acb3e7748c4066c9076b9cc9dd
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2026-02-17 16:23:11 +0900

    사용자 인증

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,13 @@
     <title>첫 화면</title>
 </head>
 <body>
-<p>안녕하세요!</p>
+<p>안녕하세요!
+    @if (Auth::check())
+        {{ \Auth::user()->name }}님</p>
+<p><a href="/logout">로그아웃</a></p>
+    @else
+        게스트님</p>
+        <p><a href="/login">로그인</a><br><a href="/register">회원 등록</a></p>
+    @endif
 </body>
 </html>
